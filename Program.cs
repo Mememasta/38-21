3d38a8b62bb2ef155cf38772d85013e550deac74
Marat <marat51098@gmail.com>
2018-09-16 11:56:52 +0400

    Редактирование и добавление комментария

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace _38_21
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!!!");//вывод текста
         }
     }
 }
